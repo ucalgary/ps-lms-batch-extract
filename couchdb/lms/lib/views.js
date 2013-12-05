c0f3935a5da4d11a2ef7157cac5e386887b3f269
King Chung Huang <kinghuang@mac.com>
2013-12-05 15:24:33 -0700

    Modified the d2l_offering view to explicitly output relationships for sections, and updated the xml4_section template to match.

--- couchdb/lms/lib/views.js
+++ couchdb/lms/lib/views.js
@@ -114,6 +114,11 @@ exports.d2l_offering = {
 				'relationships': [
 					doc['relationship']['sourcedid']['id'],				// semester (eg: 2141)
 					subject_and_number[0] + '_' + base_number			// template (eg: ACCT_217)
+				],
+				'section_relationships': [
+					lmsutils.ps_to_bb_course_code(('mapping' in doc) ? 
+					                              doc['mapping']['sourcedid']['id'] :
+					                              doc['sourcedid']['id'])
 				]
 			}
 
