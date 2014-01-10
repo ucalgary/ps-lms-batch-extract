e5fc7cb06cfb564fb1aa22574abbaa74baba2c20
King Chung Huang <kinghuang@mac.com>
2014-01-10 14:03:03 -0700

    Added an update function named member_status that will set a member's role status to the value specified in the request body.

--- couchdb/lms/lib/updates.js
+++ couchdb/lms/lib/updates.js
@@ -20,3 +20,24 @@ exports.from_ps = function(doc, req) {
 		return [null, 'No changes.'];
 	}
 }
+
+exports.member_status = function(doc, req) {
+	// If there is not an existing doc, then this is a bad request.
+	if (!doc) {
+		return [null, 'No document specified.'];
+	}
+
+	// If there is an existing doc, make sure it is a memership document,
+	// and update the role status if it is different than what is already
+	// stored.
+	if (!doc['type'] == 'member') {
+		return [null, 'Not a member document.'];
+	}
+
+	if (doc['role']['status'] == req.body) {
+		return [null, 'No changes.'];
+	}
+
+	doc['role']['status'] = req.body;
+	return [doc, 'Updated.'];
+}
