dc4f173fca7550c50dc286062c87f4507d8e1fce
Derrick Woo <dpywoo@ucalgary.ca>
2014-03-11 12:07:19 -0600

    Stop filtering by Enrollment Component and filter by Lectures and Seminars

--- couchdb/lms/lib/views.js
+++ couchdb/lms/lib/views.js
@@ -70,54 +70,60 @@ exports.d2l_template = {
 // 2 Offerings and 3 Sections
 exports.d2l_offering = {
 	map: function(doc) {
-		if (doc['type'] == 'course' && doc['grouptype']['0']['typevalue']['@level'] == '0'
-		    && (doc['grouptype']['1']['scheme'] == 'E' || doc['lmsexport']['include'] == '1')) {
+	//if (doc['type'] == 'course' && doc['grouptype']['0']['typevalue']['@level'] == '0'
+		    //&& (doc['grouptype']['1']['scheme'] == 'E' || doc['lmsexport']['include'] == '1')) {
+	if (doc['type'] == 'course' && doc['grouptype']['0']['typevalue']['@level'] == '0') {
+
 			var lmsutils = require('views/lib/lmsutils');
 			var bb_course_components = lmsutils.ps_to_bb_course_components(doc['sourcedid']['id']);
 
-			var suffix = '';
-
-			// keep Qatar templates separate so that they are organized in their
-			// own faculty/department
-			if (doc['org']['id'] == 'QA') {
-			    suffix = 'Q';
-			}
-
-			var bb_course_code = lmsutils.ps_to_bb_course_code(doc['sourcedid']['id']);
-			var semester_name = { 'P':'Spring', 'S':'Summer', 'F':'Fall', 'W':'Winter' }[bb_course_components[0]];
-			var base_number = /(\d+).*/.exec(bb_course_components[3])[1];
-			var short_prefix = bb_course_components[2] + ' '  // subject code (ENGL)
-			                 + bb_course_components[3] + ' '  // course number (201)
-			                 + bb_course_components[4]        // single character section (L, B, T, S, C, P)
-			                 + bb_course_components[5];       // section number (01)
-			var long_prefix  = short_prefix
-			                 + ' - ('
-			                 + semester_name + ' '            // semester name (Spring, Summer, Fall, Winter)
-			                 + bb_course_components[1]        // four digit year (2014)
-			                 + ') - ';
-			var translated_doc = {
+			// only output Lectures, Seminars, and exceptions
+			if(bb_course_components[4] == 'L' || bb_course_components[4] == 'S' || doc['lmsexport']['include'] == '1'){
+			    
+			    var suffix = '';
+			    
+			    // keep Qatar templates separate so that they are organized in their
+			    // own faculty/department
+			    if (doc['org']['id'] == 'QA') {
+				suffix = 'Q';
+			    }
+			    
+			    var bb_course_code = lmsutils.ps_to_bb_course_code(doc['sourcedid']['id']);
+			    var semester_name = { 'P':'Spring', 'S':'Summer', 'F':'Fall', 'W':'Winter' }[bb_course_components[0]];
+			    var base_number = /(\d+).*/.exec(bb_course_components[3])[1];
+			    var short_prefix = bb_course_components[2] + ' '  // subject code (ENGL)
+				             + bb_course_components[3] + ' '  // course number (201)
+				             + bb_course_components[4]        // single character section (L, B, T, S, C, P)
+				             + bb_course_components[5];       // section number (01)
+			    var long_prefix  = short_prefix
+				             + ' - ('
+			             	     + semester_name + ' '            // semester name (Spring, Summer, Fall, Winter)
+				             + bb_course_components[1]        // four digit year (2014)
+				             + ') - ';
+			    var translated_doc = {
 				'id': bb_course_code,
 				'section_id': bb_course_code + '_SEC',
 				'description': {
-					'short': short_prefix,
-					'long': long_prefix + doc['description']['long']
+				    'short': short_prefix,
+				    'long': long_prefix + doc['description']['long']
 				},
 				'relationships': [
-					doc['relationship']['sourcedid']['id'],				// semester (eg: 2141)
-					bb_course_components[2] + '_' + base_number + suffix		// template (eg: ACCT_217)
-				],
+						  doc['relationship']['sourcedid']['id'],				// semester (eg: 2141)
+						  bb_course_components[2] + '_' + base_number + suffix		// template (eg: ACCT_217)
+						  ],
 				'section_relationships': [
-					lmsutils.ps_to_bb_course_code(('mapping' in doc) ? 
-					                              doc['mapping']['sourcedid']['id'] :
-					                              doc['sourcedid']['id'])
-				],
+							  lmsutils.ps_to_bb_course_code(('mapping' in doc) ? 
+											doc['mapping']['sourcedid']['id'] :
+											doc['sourcedid']['id'])
+							  ],
 				'is_mapped': ('mapping' in doc)
-			}
-
-			emit(doc._local_seq, translated_doc);
-		}
-	}
-}
+			    }
+			    
+			    emit(doc._local_seq, translated_doc);
+			} // end of Lecture, Seminar, and exceptions
+	} // end of doc processing
+    } // end of map function
+} // end of function def
 
 // 4 Users
 exports.d2l_user = {
