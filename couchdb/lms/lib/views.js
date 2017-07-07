dc98873ec1597a646f9631d539113117ee315c9c
King Chung Huang <kinghuang@mac.com>
2017-07-06 23:27:28 -0600

    Use simple string parsing for expiry
    Use simple string splitting, modification, and joining to derive the
    course expiry date instead of a Date object. The JS engine on
    data.ucalgary.ca is too old for the Date class to support ISO formatted
    dates.

--- couchdb/lms/lib/views.js
+++ couchdb/lms/lib/views.js
@@ -82,10 +82,9 @@ exports.processed_courses = {
 			if ('timeframe' in doc) {
 				expiry = doc['timeframe']['end']['#text'];
 				if (expiry) {
-					d = new Date(expiry);
-					d.setFullYear(d.getFullYear() + 1);
-					expiry = d.toISOString();
-					expiry = expiry.substring(0, expiry.indexOf('T'));
+					expiry_components = expiry.split('-');
+					expiry_components[0] = (parseInt(expiry_components[0]) + 1).toString();
+					expiry = expiry_components.join('-');
 				}
 				data['dates'] = {
 					'begin': doc['timeframe']['begin']['#text'],
